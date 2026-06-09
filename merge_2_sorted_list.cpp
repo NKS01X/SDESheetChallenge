984c61844ffe40f1a5eaaab0e397ff40b9584bea
NKS01X <nikhilayush2020@gmail.com>
2026-06-09 06:39:30 +0530

    done with the merging of 2 sorted linked list

---
+++ merge_2_sorted_list.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(69);
+        ListNode* head = dummy;
+        while(l1 != nullptr && l2 != nullptr) {
+            if(l1 != nullptr && l2 != nullptr && l1->val <= l2->val) {
+                ListNode* fuck = l1;
+                l1 = l1->next;
+                dummy->next = fuck;
+                dummy = dummy->next;
+            }else if(l1 != nullptr && l2 != nullptr && l2->val <= l1->val){
+                ListNode* fuck = l2;
+                l2 = l2->next;
+                dummy->next = fuck;
+                dummy = dummy->next;
+            }
+        }
+        while(l1 != nullptr) {
+            ListNode* fuck = l1;
+            l1 = l1->next;
+            dummy->next = fuck;
+            dummy = dummy->next;
+        }
+        while(l2 != nullptr) {
+            ListNode* fuck = l2;
+            l2 = l2->next;
+            dummy->next = fuck;
+            dummy = dummy->next;
+        }
+        return head->next;
+    }
+};
